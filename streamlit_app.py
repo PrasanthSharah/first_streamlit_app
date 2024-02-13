241f422fb948de129e7fdf7da350b7b0d732a511
PrasanthSharah <159882362+PrasanthSharah@users.noreply.github.com>
2024-02-13 20:10:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('My parents new healthy dinner')
 streamlit.text('Lunch')
 streamlit.text('Non-veg Thaali')
-Streamlit.text('Kadai chicken')
+streamlit.text('Kadaichicken')
 streamlit.text('Egg omlette')
